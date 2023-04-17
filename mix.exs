795411bf595d14b90d624d7fc15c40f1670ddb71
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-04-16 22:28:05 -0400

    chore: add package details

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule CredoLanguageServer.MixProject do
   def project do
     [
       app: :credo_language_server,
+      description: "LSP implementation for Credo",
       version: "0.0.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -25,6 +27,15 @@ defmodule CredoLanguageServer.MixProject do
       {:gen_lsp, "~> 0.0.10"},
       # {:gen_lsp, path: "../gen_lsp"},
       {:credo, "~> 1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      maintainers: ["Mitchell Hanberg"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/elixir-tools/credo-language-server"},
+      files: ~w(lib LICENSE mix.exs README.md .formatter.exs)
     ]
   end
 end
