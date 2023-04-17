54ee478a50b948ddfe0aad50ffbf377de249867b
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-04-16 22:25:59 -0400

    chore: bump v0.0.1

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule CredoLanguageServer.MixProject do
   def project do
     [
       app: :credo_language_server,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: "0.0.1",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
