475c40a01a323e5d908c1288c4d602a2ce05d956
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2023-04-24 12:16:38 -0300

    ci: fix cache

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,11 @@
 {:ok, _} = Application.ensure_all_started(:credo)
-GenServer.call(Credo.CLI.Output.Shell, {:suppress_output, true})
+
+case Application.spec(:credo, :vsn) do
+  '1.5.6' ->
+    GenServer.call(Credo.CLI.Output.Shell, {:supress_output, true})
+
+  _ ->
+    GenServer.call(Credo.CLI.Output.Shell, {:suppress_output, true})
+end
 
 ExUnit.start()
