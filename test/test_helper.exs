12864ac832686ac65f37286c3914fe71ed573647
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-04-30 16:45:39 -0400

    fixup! make test deterministic on credo 1.6.7, remove 1.5.6

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,5 @@
 {:ok, _} = Application.ensure_all_started(:credo)
 
-GenServer.call(Credo.CLI.Output.Shell, {:supress_output, true})
+GenServer.call(Credo.CLI.Output.Shell, {:suppress_output, true})
 
 ExUnit.start()
