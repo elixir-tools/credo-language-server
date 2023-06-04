171f4f71e42136accf82060cb72b0d2d39f45c98
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-04 12:10:57 -0400

    ci: move credo lockfiles to test project fixture

--- test/support/project/mix.exs
+++ test/support/project/mix.exs
@@ -7,7 +7,8 @@ defmodule Project.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      lockfile: System.get_env("CREDO_LOCKFILE", "mix.lock")
     ]
   end
 
@@ -21,7 +22,7 @@ defmodule Project.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7"},
+      {:credo, "~> 1.0"},
       {:credo_contrib, "~> 0.2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
