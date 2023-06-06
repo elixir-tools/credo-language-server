b9698c4d2074bb52380c6eeb3c72b4dc97abcf2e
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-06 10:03:09 -0400

    feat: replace deps.compile with deps.loadpaths
    deps.loadpaths will noop if the deps are already compiled, instead of
    analyzing every Erlang app (which rebar3 seems to do).

--- priv/monkey/_credo_language_server_private_compiler.ex
+++ priv/monkey/_credo_language_server_private_compiler.ex
@@ -5,8 +5,15 @@ defmodule :_credo_language_server_private_compiler do
     # keep stdout on this node
     Process.group_leader(self(), Process.whereis(:user))
 
-    Mix.Task.run("deps.compile")
-    Mix.Task.run("compile")
+    # load the paths for deps and compile them
+    # will noop if they are already compiled
+    # The mix cli basically runs this before any mix task
+    # we have to rerun because we already ran a mix task
+    # (mix run), which called this, but we also passed
+    # --no-compile, so nothing was compiled, but the
+    # task was not re-enabled it seems
+    Mix.Task.rerun("deps.loadpaths")
+    Mix.Task.rerun("compile")
 
     :ok
   rescue
